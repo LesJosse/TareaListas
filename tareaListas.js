1b0c4bab241f6ad3010f7b109a0bdbb3ce99bbe4
LesJosse <josajosa97@outlook.com>
2023-01-11 20:57:18 -0500

    Subiendo tarea

---
+++ tareaListas.js
@@ -0,0 +1,14 @@
+const listaTarea = [
+  "Jose",
+  26,
+  true,
+  new Date(1996, 11, 02),
+  (libroFavorito = {
+    titulo: "Juan salvador Gaviota",
+    autor: "Richard Bach",
+    fecha: 1970,
+    url: "https://es.wikipedia.org/wiki/Juan_Salvador_Gaviota",
+  }),
+];
+
+console.log(listaTarea);
